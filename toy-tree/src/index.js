e5002b30b2f777caf87c0a0fb2345c9b597bba4a
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-07-04 17:19:49 +0900

    feat (toytree): Add  logic

--- toy-tree/src/index.js
+++ toy-tree/src/index.js
@@ -1,6 +1,8 @@
-const meow =require('meow')
+const meow = require('meow');
+const { read } = require('./read');
+const { format } = require('./format');
 
-exports.main =(argv, stdout,stderr)=>{
+exports.main = (argv, stdout, stderr) => {
   const cli = meow(
     `
     Usage
@@ -9,11 +11,26 @@ exports.main =(argv, stdout,stderr)=>{
     Examples
       $ toy-tree
       $ toy-tree path/to/dir
-    `,
+`,
     {
-      argv
-    },);
-    const dir =cli.input[0] ||'.';
-    stdout(dir)
-    return 0
-}
+      argv,
+    },
+  );
+
+  const dir = cli.input[0] || '.';
+
+  let root;
+
+  try {
+    root = read(dir);
+  } catch (e) {
+    stderr(`Error: ${e.message}`);
+    return 1;
+  }
+
+  const output = format(root);
+
+  stdout(output);
+
+  return 0;
+};
